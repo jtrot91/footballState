d6f19435c6e65a061788b94544515b49324bf410
James <jtrot91@bellsouth.net>
2018-08-03 17:41:54 -0400

    First commit

--- cfbStates.py
+++ cfbStates.py
@@ -1,1 +1,19 @@
-print("Hello World")
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def makeSoup(url):
+	driver = webdriver.Firefox()
+	driver.get(url)
+	html = driver.page_source
+	driver.close()
+	return BeautifulSoup(html, "html.parser")
+
+if __name__ == "__main__":
+	teamPage = makeSoup("http://www.winsipedia.com/team")
+	teams = teamPage.findAll('a', href=True)
+	print(teams)
