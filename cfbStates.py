a54724decf99c016301dab6ae94d4f5ad08e9dcb
James <jtrot91@bellsouth.net>
2018-08-10 17:56:24 -0400

    First real update

--- cfbStates.py
+++ cfbStates.py
@@ -1,19 +1,29 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
+import re
 
-def makeSoup(url):
-	driver = webdriver.Firefox()
-	driver.get(url)
-	html = driver.page_source
-	driver.close()
-	return BeautifulSoup(html, "html.parser")
+def getTeamInfo(teamlink):
+	page = requests.get(teamlink)
+	content = page.content
+	soup = (BeautifulSoup(content, "html.parser"))
+	teamSection = soup.find("div", {"class": "row-fluid matchupslist row4"})
+	eachTeam = teamSection.find_all("li")
+	for team in eachTeam:
+		teamDiv = team.div
+		if(teamDiv['data-conference'] != "NON-FBS TEAMS"):
+			#GET STATE OF TEAM
+			#ADD DATA
+			#print(teamDiv['data-name'] + ' ' + teamDiv['data-winscount'] + ' ' + teamDiv['data-lossescount'] + ' ' + teamDiv['data-tiescount'])
+			
+
 
 if __name__ == "__main__":
-	teamPage = makeSoup("http://www.winsipedia.com/team")
-	teams = teamPage.findAll('a', href=True)
-	print(teams)
+	soup = BeautifulSoup(open("teampage.html"), "html.parser")
+	teams = soup.find_all("li", "span4") + soup.find_all("li", "span3")
+	teamURLs = []
+	for li in teams:
+		li = str(li)
+		teampage = re.search('href="(.*)">', li)
+		teamURLs.append(teampage.group(1))
+	getTeamInfo(teamURLs[0])
+	
